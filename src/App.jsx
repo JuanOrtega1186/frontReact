d3d721e1c929f2d5be1deca213f60ac71fd331df
jperezr-psl <juan.perezr@perficient.com>
2021-10-29 14:44:09 -0500

    improvements

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,6 @@ import theme from './temaConfg';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link,
-  NavLink
 } from "react-router-dom";
 
 function App() {
--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import * as React from 'react';
+import {
+  makeStyles,
+} from "@material-ui/core";
+import CustomCard from "./CustomCard";
+
+const useStyles = makeStyles((theme) => ({
+  offset: theme.mixins.toolbar,
+  container: {
+    display: 'flex',
+  },
+}));
 
 const HomePage = () => {
-    return (
-        <div>
-            inicio
-        </div>
-    )
-}
+  const classes = useStyles();
+  return (
+    <div className={classes.container}>
+      <CustomCard />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
