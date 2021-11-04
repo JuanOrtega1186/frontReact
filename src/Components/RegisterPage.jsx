6c28ae536742038c9fbd5833856fc3ac5c53514b
Cristian Henao <analista3@mic.com.co>
2021-11-04 13:58:55 -0500

    conexion al back con axios, guardado y logueo en localstorage

--- src/Components/RegisterPage.jsx
+++ src/Components/RegisterPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState}from 'react';
 import {Grid, Paper, Avatar, Typography, TextField, Button} from '@material-ui/core'
 import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined';
 import Radio from '@material-ui/core/Radio';
@@ -7,12 +7,88 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import axios from 'axios';
+import Swal from 'sweetalert2'
+import {urlGuardar, urlhome} from '../service/url'
 
 const RegisterPage = () => {
 const paperStyle={padding:'30px 50px', width:500, margin:"20px auto"}
 const headerStyle={margin:0}
 const avatarStyle={backgroundColor:'blue'}
 const marginTop = {marginTop:5}
+
+const [detal, setDetal] = useState([])
+const [name, setName] = useState('')
+const [email, setEmail] = useState('')
+const [country, setCountry] = useState('')
+const [city, setCity] = useState('')
+const [passWord, setPassword] = useState('')
+const [shop, setShop] = useState('')
+
+const register = () =>{
+    if(!name || !email || !country || !city || !passWord){
+        Swal.fire({
+            title: 'Warning!',
+            text: 'Please fill in all the fields ',
+            icon: 'warning',
+            confirmButtonText: 'Cool'
+        })
+    }else{
+        let profile = ('')
+        let body = {
+            name: name,
+            email: email,
+            country: country, 
+            city: city,
+            password: passWord,
+            profile: profile,
+            shopName: shop
+        }
+        axios.post(urlGuardar + 'users', body)
+    
+        .then(res => {
+            // eslint-disable-next-line no-lone-blocks
+            { Object.entries(res.data).length !== 0 && validator(res.data) }
+        }).catch(
+            function(error) {
+                debugger
+                Swal.fire({
+                    title: 'error!',
+                    text: error,
+                    icon: 'error',
+                    confirmButtonText: 'Fine'
+                  })
+            }
+        )
+    }
+
+    const validator = (message) => {
+        Swal.fire({
+            title: 'Mensaje!',
+            text: message.message,
+            icon: message.icon,
+            confirmButtonText: 'Cool'
+          })
+    }
+
+    const warning = (data) => {
+        debugger
+        if(data[0].email != null)
+        {
+
+            setDetal(data)
+            let datos = data
+            console.log(detal)
+            console.log(datos)
+            window.location.href= urlhome
+        }else {
+            warning(data)
+        }
+       
+       
+        
+    }
+}
 
     return (
         <div className = "container-fluid">
@@ -26,22 +102,22 @@ const marginTop = {marginTop:5}
                         <Typography variant='caption' gutterBottom>Please fill this form to create an account!</Typography>
                     </Grid>
                     <form>
-                        <TextField fullWidth label = 'Name' placeholder = 'Insert your name'/>
-                        <TextField fullWidth label = 'Email' placeholder = 'Insert your email address'/>
-                        <TextField fullWidth label = 'Country' placeholder = 'Insert your Country'/>
-                        <TextField fullWidth label = 'City' placeholder = 'Insert your City'/>
-                        <TextField fullWidth label = 'Password' type ="password" placeholder = 'Insert your password'/>
+                        <TextField fullWidth label = 'Name' onChange={(e)=>setName(e.target.value)} placeholder = 'Insert your name'/>
+                        <TextField fullWidth label = 'Email' onChange={(e)=>setEmail(e.target.value)} placeholder = 'Insert your email address'/>
+                        <TextField fullWidth label = 'Country' onChange={(e)=>setCountry(e.target.value)} placeholder = 'Insert your Country'/>
+                        <TextField fullWidth label = 'City' onChange={(e)=>setCity(e.target.value)} placeholder = 'Insert your City'/>
+                        <TextField fullWidth label = 'Password' onChange={(e)=>setPassword(e.target.value)} type ="password" placeholder = 'Insert your password'/>
                         <FormControl component="fieldset" style = {marginTop}>
                         <FormLabel component="legend">Profile</FormLabel>
                             <RadioGroup aria-label="profile" name="profile" style={{display:'initial'}}>
-                                <FormControlLabel value="owner" control={<Radio />} label="Owner" />
-                                <FormControlLabel value="buyer" control={<Radio />} label="Buyer" />
+                                <FormControlLabel value="owner"  control={<Radio />} label="Owner" />
+                                <FormControlLabel value="buyer"  control={<Radio />} label="Buyer" />
                             </RadioGroup>
-                        <TextField fullWidth label = 'Shopname' placeholder = 'Insert your Shop Name'/>
+                        <TextField fullWidth label = 'Shopname' onChange={(e)=>setShop(e.target.value)} placeholder = 'Insert your Shop Name'/>
                         </FormControl>
                         <FormControlLabel control={<Checkbox name="checkedA" />} 
                         label="I accept Terms and Conditions."/>                 
-                        <Button type = 'submit' variant='contained' color = 'primary' fullWidth>Sing Up</Button>
+                        <Button type = 'button' variant='contained' onClick={()=>register()} color = 'primary' fullWidth>Sing Up</Button>
                     </form>
                 </Paper>
             </Grid>
---
+++ src/service/url.jsx
@@ -0,0 +1,10 @@
+
+
+//Url's de el backend 
+export const urlConsultarEmail= ('http://localhost:3000/api/');  
+export const urlGuardar= ('http://localhost:3000/api/')
+
+
+//Url's de el Front
+export const urlhome = ('http://localhost:3001')
+
